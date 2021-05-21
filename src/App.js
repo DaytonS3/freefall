2cfb55ea4c50624077832c1e6f9c554c4b0285ec
test kk <00@oo>
2021-05-21 12:19:19 -0400

    added email func

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
-import image from './images/home-trans.png'
-
 import Landing from "./components/landing";
+import Footer from "./components/footer";
 
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" render={() => <Landing />} />
-      <Title />
+      <Footer />
     </div>
   );
 
-  function Title() {
-    return(
-      <div id="text_div center_all">
-        <div className="center_all_text">
-          <p className="homeHeader">GO FREEFALL</p>
-          <p className="homeText">Modernizing The Skydive Industy</p>
-          <div style={{height: "2px", width: "600px", background: "white", margin: "0 auto", marginTop: "10px", opacity: "20%"}}></div>
-        </div>
-
-        <div className="center_all">
-          <img className="homeImage" src={image} alt="LOGO"/>
-        </div>
-
-        <div className="center_all_input">
-          <p className="formText">Want to be first to know when we're ready?</p>
-          <div className="formContainer"> 
-            <div className="form__group field">
-              <input className="form__field"  />
-            </div>
-            <p className="formBtn">SUBSCRIBE</p>
-          </div>
-          
-        </div>
-        
-      </div>
-    )
-  }
+  
 }
 
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import image from '../images/home-trans.png'
+import emailjs from 'emailjs-com';
+
+
+
+
+class Footer extends React.Component{
+    
+    sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_8nay5fx', 'template_95loram', e.target, 'user_7LqYoIj0HcA9VaZNbz3EP')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      }
+    
+
+    render(){
+        return(
+        <div id="text_div center_all">
+            <div className="center_all_text">
+            <p className="homeHeader">GO FREEFALL</p>
+            <p className="homeText">Modernizing The Skydive Industy</p>
+            <div style={{height: "2px", width: "600px", background: "white", margin: "0 auto", marginTop: "10px", opacity: "20%"}}></div>
+            </div>
+
+            <div className="center_all">
+            <img className="homeImage" src={image} alt="LOGO"/>
+            </div>
+
+            <div className="center_all_input">
+            <p className="formText">Want to be first to know when we're ready?</p>
+            <form autocomplete="off" className="formContainer" onSubmit={this.sendEmail}> 
+                <div className="form__group field">
+                    <input className="form__field" name="email" />
+                </div>
+                <input className="formBtn" type="submit" value="SUBSCRIBE"/>
+            </form>
+            
+            </div>
+            
+        </div>
+        )
+    }
+    
+  }
+
+export default Footer;
